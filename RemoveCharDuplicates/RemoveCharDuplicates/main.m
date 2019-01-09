53217b7bfc35768ffe17009918cf14479875aab3
Ion Timotin <timotin.ion89@gmail.com>
2019-01-09 13:31:37 -0600

    Interview Question
    To solve in O(1) complexity, and O(N) running time, used block for processig string in the NSRange, inside the block checked if NSMutableSet NOT containsObject each char apart. if it is not contained just added inside the set. The similar I used in Java with help of HashTable dictionary type.
    Option I usedd NSStringEnumerationByComposedCharacterSequences

---
+++ RemoveCharDuplicates/RemoveCharDuplicates/main.m
@@ -0,0 +1,36 @@
+//
+//  main.m
+//  RemoveCharDuplicates
+//
+//  Created by Timotin Ion on 1/8/19.
+//  Copyright © 2019 Timotin. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+//#include <stdio.h>
+//#include <objc/objc.h>
+
+NSString* removeDuplicates(NSString* input){
+    NSMutableSet *seenCharacters = [NSMutableSet set];
+    NSMutableString *result = [NSMutableString string];
+    [input enumerateSubstringsInRange:NSMakeRange(0, input.length) options:NSStringEnumerationByComposedCharacterSequences usingBlock:^(NSString *substring, NSRange substringRange, NSRange enclosingRange, BOOL *stop) {
+        if (![seenCharacters containsObject:substring]) {
+            [seenCharacters addObject:substring];
+            [result appendString:substring];
+        }
+    }];
+    return result;
+}
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+    
+        NSString *input = @"addbcddaa";
+        printf("Initial string: %s \n", [input UTF8String]);
+        NSString *result = removeDuplicates(input);
+        printf("After removing duplicates:\n%s \n", [result UTF8String]);
+    }
+    return 0;
+}
+
