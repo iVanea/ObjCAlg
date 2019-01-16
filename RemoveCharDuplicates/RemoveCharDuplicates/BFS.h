917da96451bf2a32d2738f0358cdc3da9c44aecf
Ion Timotin <timotin.ion89@gmail.com>
2019-01-16 16:32:43 -0600

    threads
    Need to implement threads, for really long strings

---
+++ RemoveCharDuplicates/RemoveCharDuplicates/BFS.h
@@ -0,0 +1,18 @@
+//
+//  BFS.h
+//  RemoveCharDuplicates
+//
+//  Created by Timotin Ion on 1/16/19.
+//  Copyright © 2019 Timotin. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface BFS : NSObject
+
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ RemoveCharDuplicates/RemoveCharDuplicates/BFS.m
@@ -0,0 +1,13 @@
+//
+//  BFS.m
+//  RemoveCharDuplicates
+//
+//  Created by Timotin Ion on 1/16/19.
+//  Copyright © 2019 Timotin. All rights reserved.
+//
+
+#import "BFS.h"
+
+@implementation BFS
+
+@end
